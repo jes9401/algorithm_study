376f40e09f1408a2cc609b92fbacb691e4cfc163
jes <jes9401@nate.com>
2021-05-25 15:49:15 +0900

    pro 05/25

--- pro_kakao8.py
+++ pro_kakao8.py
@@ -1,34 +1,26 @@
 # 프로그래머스 카카오
 # 오픈채팅방 https://programmers.co.kr/learn/courses/30/lessons/42888
-# 미해결
+# 해결 완료
 
 def solution(record):
     answer = []
     dic = {}
     for i in record:
+        # 명령어, 아이디, 이름으로 구분해서 저장
         temp = i.split()
+        # 문자열에 공백 넣은 이유 => 밑에서 split으로 구분할거라서
         if temp[0] == "Enter":
-            answer.append("%s %s 님이 들어왔습니다." %(temp[1],temp[2]))
+            answer.append("%s 님이 들어왔습니다." %(temp[1]))
             dic[temp[1]] = temp[2]
         elif temp[0] == "Leave":
-            answer.append("%s %s 님이 나갔습니다." %(temp[1],dic[temp[1]]))
+            answer.append("%s 님이 나갔습니다." %(temp[1]))
         elif temp[0] == "Change":
             dic[temp[1]] = temp[2]
-    answer2 = answer.copy()
     for j in range(len(answer)):
         s0 = answer[j].split()[0] # 아이디
-        s1 = answer[j].split()[1] # 이름
-        if  s1 == dic[s0]:
-            answer[j] = answer[j].replace(s0,"")
-            answer[j] = answer[j].lstrip()
-            answer[j] = answer[j].replace(answer[j][len(s1)],'')
-        else:
-            answer[j] = answer[j].replace(s0,"")
-            answer[j] = answer[j].lstrip()
-            answer[j] = answer[j].replace(s1, dic[s0])
-            answer[j] = answer[j].replace(answer[j][len(dic[s0])],'')
-        if "님이 " not in answer[j]:
-            answer[j] = answer[j].replace("님이","님이 ")
+        # '아이디 ' 인 형식 변경 => dic[아이디] == (이름) 
+        answer[j] = answer[j].replace(s0+' ',dic[s0])
     return answer
 
-print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]))
+print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234",
+                "Enter uid1234 Prodo","Change uid4567 Ryan"]))
