9fea6ca5e4c2cc478dcb85a5f562aeb43065c3ba
정의석 <jes9401@nate.com>
2021-08-14 15:28:28 +0900

    pro 08/14

---
+++ pro_test21.py
@@ -0,0 +1,13 @@
+def solution(name_list):
+    temp = {}
+    num = 65
+    for i in range(len(name_list)):
+
+        if name_list[i] not in temp:
+            temp[name_list[i]] = 0
+            name_list[i] += chr(num)
+        else:
+            temp[name_list[i]] = temp[name_list[i]] + 1
+            name_list[i] += chr(temp[name_list[i]] + num)
+
+    return name_list
